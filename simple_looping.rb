a60a5bdc4b716369a79231a5ba696789c76d455c
Dennis <dennis.basyrov@gmail.com>
2019-05-03 03:24:10 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
     c += 1
-    if counter >= 10 
+    if counter >= number_of_times
     break
 end
   end  
