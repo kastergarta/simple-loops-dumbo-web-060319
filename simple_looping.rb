eade6de19413f46b9b9b1dab9d2c76d5ad0ec4a5
Dennis <dennis.basyrov@gmail.com>
2019-05-03 03:43:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -31,7 +31,7 @@ end
 
 def while_iterator(number_of_times)
   i = 0
-  while i > number_of_times
+  while i < number_of_times
   puts "Welcome to Flatiron School's Web Development Course!"
   i += 1
 end
