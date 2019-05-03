1474c8a4f46378bc1fdf7971f6a8260d9b1b99af
Dennis <dennis.basyrov@gmail.com>
2019-05-03 03:33:04 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,8 +9,7 @@ def loop_iterator(number_of_times)
     break
 end
   end  
-  
-  loop_iterator(7)
+
   
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
